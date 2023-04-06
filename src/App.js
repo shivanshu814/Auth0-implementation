a991a602da004535ba72f8705830f5b316ce9b43
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-04-07 00:46:51 +0530

    Auth0 Login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 /* @format */
+import React from 'react';
+import './App.css';
+import LoginButton from './components/LoginButton';
 function App() {
-	return <div></div>;
+	return <LoginButton></LoginButton>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+// const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+// const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
+	<Auth0Provider
+		domain='dev-0wrfw176zqvtalfl.us.auth0.com'
+		clientId='7cXyjkhOZ2xoMeN1kfOkn46srqlSMJgt'
+		authorizationParams={{
+			redirect_uri: window.location.origin,
+		}}>
 		<App />
-	</React.StrictMode>
+	</Auth0Provider>
 );
